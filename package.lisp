6d136cbde54fa13358eecc63221032ea5c3c265a
common-lamb <common.lamb@gmail.com>
2026-05-07 15:53:13 -0500

    works more gooder

--- package.lisp
+++ package.lisp
@@ -5,8 +5,20 @@
   (:use #:cl)
   (:export
    ;; parameters
-   *&&&*
-
+   *project-name*
+   *operations-home*
+   *remote-login*
+   *target-dir*
+   *payload-files*
    ;; functions
-   &&&
+   create-transfer-directories
+   create-documents
+   test-transfer
+   check-payload-files
+   link-all
+   hash-all
+   compress
+   transfer
+   cleanup
+   deliver-payload
    ))
--- payload.lisp
+++ payload.lisp
@@ -2,178 +2,262 @@
 
 ;;;; check dependencies
 (defun check-dependencies ()
+  (cmd:$cmd "which tar")
   (cmd:$cmd "which rsync")
   (cmd:$cmd "which md5sum")
   ;; all good
   T)
 
+(defparameter *project-name* nil
+  "name to prepend &&&")
 
 (defparameter *remote-login* ""
-  "
-like: user@remote: with trailing colon
-or empty string if local
-")
+  "string like: user@remote:
+with trailing colon or empty string if local")
 
-(defparameter *target* nil
-  "
-an absolute file path as string with trailing slash")
-
-(defparameter *payload* nil
-  "list of file paths (no dirs)")
-
+(defparameter *target-dir* nil
+  "an absolute file path to a directory as string with trailing slash")
+(defparameter *payload-files* nil
+  " a list of file paths (no dirs)")
+(defparameter *payload-dirs* nil
+  "&&& not implemented list of file paths to dirs (no files))")
 (defparameter *operations-home* (user-homedir-pathname)
-  "
-used to direct operations to a consistent place")
+  "pathname used as toplevel of staging")
 
+(defun check-parameters ()
+  (let ((params '(*project-name*
+                  *operations-home*
+                  *remote-login*
+                  *target-dir*)))
+    ;; print current state
+    (mapcar (lambda (p) (format t "~&~A: ~A~%" (symbol-name p) (symbol-value p)))
+            params)
+    ;; none nil
+    (assert (every (lambda (p) (not (null (symbol-value p))))
+                   params)
+            (*project-name*
+             *operations-home*
+             *remote-login*
+             *target-dir*) "parameters must be set and cannot be nil.")
 
-;;;; create dir structure
+    ;; "" or ends with :
+    (assert (or (str:ends-with? ":" *remote-login*)
+                (string-equal "" *remote-login*))
+            (*remote-login*)
+            "*remote-login* can be an empty string for local transfers, if set it must end with :")
+    ;; sends with /
+    (assert (and (or (str:starts-with? "/" *target-dir*)
+                     (str:starts-with? "~/" *target-dir*))
+                 (str:ends-with? "/" *target-dir*))
+            (*target-dir*)
+            "*target-dir* must start with either ~ ~/ and must end with /")
+    ;; must have some payload
+    (assert (not (null *payload-files*))
+            (*payload-files*)
+            "*payload-files* must be a list with a filepath in it")
+    ))
 
-(defun direct-parent (path)
+;;;; create dir names
+
+(defun parent-namestring (path)
   "
 takes a path
-returns the single lowest directory namestring
-"
+returns the single lowest directory namestring "
   (first (last (pathname-directory path))))
-(defparameter *date*
-  (local-time:format-timestring nil (local-time:now) :format local-time:+iso-8601-date-format+))
-(defun transfer-dir-name ()
-  (format nil "~A_data-transfer/" *date*))
 
-(defun transfer-dir ()
-  (filepaths:join *operations-home* (transfer-dir-name)))
-(defun payload-dir ()
-  (filepaths:join (transfer-dir) "payload/" ))
-(defun docs-dir ()
-  (filepaths:join (transfer-dir) "docs/" ))
-(defun hashes-dir ()
-  (filepaths:join (transfer-dir) "hashes/" ))
-(defun compressed-name ()
-  (str:concat (direct-parent (transfer-dir)) ".tar.gz"))
+(defun make-transfer-dir-name ()
+  (format nil "~A_data-transfer/" *project-name*))
 
+(defun make-transfer-dir ()
+  "dir for all collection and operation before sending"
+  (filepaths:join *operations-home* (make-transfer-dir-name)))
+(defun make-payload-dir ()
+  "dir for collecting *payload-files* into"
+  (filepaths:join (make-transfer-dir) "payload/" ))
+(defun make-docs-dir ()
+  "dir for docs"
+  (filepaths:join (make-transfer-dir) "docs/" ))
+(defun make-hashes-dir ()
+  "dir for &&&"
+  (filepaths:join (make-transfer-dir) "hashes/" ))
+(defun make-compressed-name ()
+  (str:concat (parent-namestring (make-transfer-dir)) ".tar.gz"))
 
+;;;; create dir structure
 (defun create-transfer-directories ()
-  (ensure-directories-exist (transfer-dir))
-  (ensure-directories-exist (payload-dir))
-  (ensure-directories-exist (docs-dir))
-  (ensure-directories-exist (hashes-dir)))
-
+  (check-dependencies)
+  (check-parameters)
+  (ensure-directories-exist (make-transfer-dir))
+  (ensure-directories-exist (make-payload-dir))
+  (ensure-directories-exist (make-docs-dir))
+  (ensure-directories-exist (make-hashes-dir))
+  (ensure-directories-exist (make-transfer-dir)))
 
 ;;;; define commands
-(defparameter *cmd-compress* "tar -czvf")
+(defparameter *cmd-compress* "tar -chzvf") ;; -h to derefrence symlinks!
 (defparameter *cmd-decompress* "tar -xzvf")
 (defparameter *cmd-hash* "md5sum * > ../hashes/checksum.md5")
-(defparameter *cmd-hash-check* "md5sum -c")
+(defparameter *cmd-hash-check* "md5sum --check")
 
 (defun call-rsync (file target &key (flags "-vrPL --dry-run"))
   "
 ARGS:
 file: string like /path/to/file.type
 target: string like user@remote:/path/to/dest/
-flags: string like -vrPLn
+flags: string like -vrPL --dry-run
 "
-  (uiop:run-program (format nil "rsync ~A ~A ~A" flags file target)
-                       :output :interactive
-                       :input :interactive))
+
+  (handler-case
+      (uiop:run-program (format nil "rsync ~A ~A ~A" flags file target)
+                        :output :interactive
+                        :input :interactive)
+    (error (e)
+      (format t "~&rsync Error: ~A~%" e)
+      (format t "~&Error codes:~%")
+      (format t "~&    code 23 if no write access~%")
+      (format t "~&    code 3 if dir doesn't exist~%")
+      (format t "~&    code 255 if *remote-connection* is wrongo bongo~%")
+      )))
 
 ;;;; documents
-(defun make-documents ()
-  "
-creates the documents for the recievers
-"
+(defun create-documents ()
+  " creates simple documents for the end users"
   ;; make decompress doc file
-  (with-open-file (out (filepaths:join (docs-dir) "compression-docs.txt" )
+  (with-open-file (out (filepaths:join (make-docs-dir) "compression-docs.txt" )
                        :direction :output
                        :if-exists :supersede
                        :if-does-not-exist :create)
     (write-line (format nil "Greetings!~%") out)
     (write-line (format nil "compressed with:~%  ~A <directory>" *cmd-compress*) out)
-    (write-line (format nil "decompress with:~%  ~A <compressed>.tar.gz" *cmd-decompress*) out))
+    (write-line (format nil "decompress with:~%  ~A <compressed-directory>.tar.gz" *cmd-decompress*) out))
 
   ;; make hash doc file
-  (with-open-file (out (filepaths:join (docs-dir) "hash-docs.txt" )
+  (with-open-file (out (filepaths:join (make-docs-dir) "hash-docs.txt" )
                        :direction :output
                        :if-exists :supersede
                        :if-does-not-exist :create)
-    (write-line (format nil "With great regards!~%") out)
+    (write-line (format nil "With our greatest regards!~%") out)
     (write-line (format nil "hashed with:~%  ~A" *cmd-hash*) out)
     (write-line (format nil "nav to payload dir and check hashes with:~%  ~A ../hashes/checksum.md5 " *cmd-hash-check*) out)
     T))
 
-
 ;;;; check target has write access
+
+(defun test-ssh-connection ()
+  "Test if SSH connection works with verbose output"
+  (when (not (string-equal "" *remote-login*))
+    (let ((host (string-right-trim ":" *remote-login*)))
+      (format t "~&Testing SSH connection to: ~A~%" host)
+      ;; Add -v for verbose SSH output
+      (handler-case
+          (uiop:run-program (format nil "ssh -v ~A 'echo SSH_OK'" host)
+                            :output :interactive
+                            :error-output :interactive)
+        (error (e)
+          (format t "~&SSH Error: ~A~%" e)
+          (format t "~&Try running manually: ssh ~A~%" host))))))
+
 (defun test-transfer ()
-  "
-rsync a file to target to test
-errors on code 23 if no write access
-"
-  (let ((target (format nil "~A~A" *remote-login* *target*))
-        (file (namestring (filepaths:join (docs-dir) "compression-docs.txt"))))
+  (let* (
+         (file (namestring (filepaths:join (make-docs-dir) "compression-docs.txt")))
+         (target (format nil "~A~A~A"
+                         *remote-login*
+                         *target-dir*
+                         (filepaths:name file)))
+         )
+    (test-ssh-connection)
+    (format t "~&Copying file:~&  ~A" file)
+    (format t "~&Testing target dir write access:~&  ~A" target)
+    (call-rsync file target :flags "-vP")
+    ))
 
-    (format t "Testing target dir write access:~&  ~A" target)
-    (call-rsync file target :flags "-vrPL")))
+;;;; file operations, link and hash
 
+(defun check-payload-files ()
+  "check each file in *payload-files* exists"
+  ;; print current state
+  (if (null *payload-files*)
+      (warn "*payload-files* is nil"))
+  (mapcar (lambda (f) (format t "~&file: ~A existsp: ~A~%" f (probe-file f)))
+          *payload-files*)
 
-;;;; check each in payload exists &&&
-;; (mapcar #'probe-file *payload*)
+  (assert (every (lambda (f) (not (null (probe-file f))))
+                 *payload-files*)
+          () "all payload files must exist"))
 
-;;;; symlink each in payload into payload dir &&&
 (defun make-symlink (file)
-  "
-creates a symlink in payload
-"
-  (cmd:cmd (format nil
-                   "ln -s '~A' '~A'"
-                   (namestring
-                    (uiop:ensure-absolute-pathname file))
-                   (namestring
-                    (payload-dir)))))
+  " creates a symlink in payload "
+  (format t "~&Linking:~%  '~A'" file)
+  (let* (
+         (real (namestring
+             (uiop:ensure-absolute-pathname file)))
+         (link (namestring
+             (merge-pathnames (filepaths:name file)
+                              (make-payload-dir))))
+         )
+    (org.shirakumo.filesystem-utils:create-symbolic-link link real)))
 
-(defun link-payload ()
-  "
-symlinks all files in payload list into the payload dir
-"
-  (mapcar #'make-symlink *payload*))
-
-;;;; hash each in payload into hashes dir
+(defun link-all ()
+  " symlinks all files in payload list into the payload dir "
+  (mapcar #'make-symlink *payload-files*))
 
 (defun hash-file (file)
+  "hash a file in payload into hashes dir"
   (format t "~&Hashing:~%  '~A'" file)
   (cmd:cmd (format nil
                    "md5sum '~A' >> ../hashes/checksum.md5"
-                   (namestring file))))
+                   (filepaths:name file))))
 
-(defun hash-payload ()
-  (uiop:with-current-directory  ((payload-dir))
+(defun hash-all ()
+  (uiop:with-current-directory  ((make-payload-dir))
 
-    (cmd:cmd "echo > ../hashes/checksum.md5") ;; clean start
+    (cmd:cmd "echo '# made with lisp' > ../hashes/checksum.md5") ;; clean start
     (mapcar #'hash-file
               (uiop:directory-files (uiop:getcwd)))))
 
 ;;;; recursive compress data-transfer dir
 
-(defun call-compress ()
+(defun compress ()
   "
 calls cmd-compress on the transfer dir, creating the new name
 "
   (uiop:with-current-directory (*operations-home*)
-    (let ((payload-name (namestring (transfer-dir))))
-      (cmd:cmd (format nil "~A '~A' '~A'" *cmd-compress* (compressed-name) payload-name))
-      )))
+    (let ((payload-name (pathname-utils:directory-name (make-transfer-dir))))
+      (cmd:cmd (format nil "~A '~A' '~A'"
+                       *cmd-compress* (make-compressed-name) payload-name)))))
 
 ;;;; move dir to target
-(defun do-transfer ()
-  "
-rsync the compressed data to target
-"
-  (let ((target (format nil "~A~A" *remote-login* *target*)))
+(defun transfer ()
+  " rsync the compressed data to target "
+  (let (
+        (file (make-compressed-name))
+        (target (format nil "~A~A" *remote-login* *target-dir*))
+        )
 
-    (format t "Transfering to target dir:~&  ~A" target)
+    (format t "~&Copying file:~&  ~A" file)
+    (format t "~&Transferring to target dir:~&  ~A" target)
+
     (uiop:with-current-directory (*operations-home*)
-      (call-rsync (compressed-name) target :flags "-vP")
-      )))
+      (call-rsync file target :flags "-vrPL"))
+    ))
+
 
 ;;;; clean up
 (defun cleanup ()
-  (org.shirakumo.filesystem-utils:delete-directory (transfer-dir)  )
+  (org.shirakumo.filesystem-utils:delete-directory (make-transfer-dir)  )
   (uiop:with-current-directory (*operations-home*)
-    (org.shirakumo.filesystem-utils:delete-file* (compressed-name))))
+    (org.shirakumo.filesystem-utils:delete-file* (make-compressed-name))))
+
+(defun deliver-payload ()
+  "perform all operations to transfer payload"
+  (create-transfer-directories)
+  (create-documents)
+  (test-transfer)
+  (check-payload-files)
+  (link-all)
+  (hash-all)
+  (compress)
+  (transfer)
+  (cleanup)
+  (format t "~&~%payload delivered")
+  )
